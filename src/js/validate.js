80d05db341f12c048477f6cb09549a1e088cc453
Max Ratajczak <75450072+maxratajczak@users.noreply.github.com>
2021-04-09 21:27:01 -0400

    Add files via upload

---
+++ src/js/validate.js
@@ -0,0 +1,44 @@
+function validate(event) {
+  // TODO - write custom validation logic to validate the longitude and latitude
+  // values. The latitude value must be a number between -90 and 90; the
+  // longitude value must be a number between -180 and 180. If either/both are
+  // invalid, show the appropriate error message in the form, and stop the 
+  // form from being submitted. If both values are valid, allow the form to be
+  // submitted.
+
+  //Getting the values of the lat and long input
+  var lat = parseInt(document.querySelector("#lat").value);
+  var long = parseInt(document.querySelector("#long").value);
+
+  //Selecting the error messages in the html
+  var latError = document.getElementById("errorLat");
+  var longError = document.getElementById("errorLong");
+
+  //Resetting the error messages so it can recheck values & show error to proper invalid value
+  resetErrors(latError, longError);
+
+  //Validating the lat and long
+  if(isNaN(lat) || (lat < -90 || lat > 90)){
+    showError(latError);
+    return false;
+  }
+  else if(isNaN(long) || (long < -180 || long > 180)){
+    longError.style.display = "inline-block";
+    return false;
+  }
+}
+
+function showError(element){
+  element.style.display = "inline-block";
+}
+
+function resetErrors(error1, error2){
+  error1.style.display = "none";
+  error2.style.display = "none";
+}
+
+// Wait for the window to load, then set up the submit event handler for the form.
+window.onload = function() {
+  const form = document.querySelector('form');
+  form.onsubmit = validate;
+};
